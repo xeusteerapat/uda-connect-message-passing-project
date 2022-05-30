ea06e0d64f2f63316f232d3d52e29407ed50cc80
xeusteerapat <xeus085@gmail.com>
2022-05-30 23:09:36 +0700

    :sparkles: [Update] update schema person-connection

--- modules/persons-connection/app/udaconnect/schemas.py
+++ modules/persons-connection/app/udaconnect/schemas.py
@@ -1,5 +1,7 @@
-from app.udaconnect.models import Person
+from app.udaconnect.models import Location, Person
+from geoalchemy2.types import Geometry
 from marshmallow import Schema, fields
+from marshmallow_sqlalchemy.convert import ModelConverter
 
 
 class PersonSchema(Schema):
@@ -10,3 +12,19 @@ class PersonSchema(Schema):
 
     class Meta:
         model = Person
+
+
+class LocationSchema(Schema):
+    id = fields.Integer()
+    person_id = fields.Integer()
+    longitude = fields.String(attribute="longitude")
+    latitude = fields.String(attribute="latitude")
+    creation_time = fields.DateTime()
+
+    class Meta:
+        model = Location
+
+
+class ConnectionSchema(Schema):
+    location = fields.Nested(LocationSchema)
+    person = fields.Nested(PersonSchema)
