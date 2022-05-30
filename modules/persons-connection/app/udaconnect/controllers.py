721a6c0bb8a947bb0390e96c68de7399c5dd5e05
xeusteerapat <xeus085@gmail.com>
2022-05-30 23:35:30 +0700

    :sparkles: [Update] update controllers

--- modules/persons-connection/app/udaconnect/controllers.py
+++ modules/persons-connection/app/udaconnect/controllers.py
@@ -1,9 +1,11 @@
 from datetime import datetime
-from app.udaconnect.models import Person
+
+from app.udaconnect.models import Connection, Person
 from app.udaconnect.schemas import (
+    ConnectionSchema,
     PersonSchema,
 )
-from app.udaconnect.services import PersonService
+from app.udaconnect.services import ConnectionService, PersonService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
@@ -16,15 +18,14 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 
 # TODO: This needs better exception handling
 
-
-#! Separate to "Person Service"
 @api.route("/persons")
 class PersonsResource(Resource):
-    @accepts(schema=PersonSchema)
-    @responds(schema=PersonSchema)
     def post(self) -> Person:
-        payload = request.get_json()
+        payload = {'first_name': request.args.get('first_name'), 'last_name': request.args.get(
+            'last_name'), 'company_name': request.args.get('company_name')}
+
         new_person: Person = PersonService.create(payload)
+
         return new_person
 
     @responds(schema=PersonSchema, many=True)
@@ -40,3 +41,26 @@ class PersonResource(Resource):
     def get(self, person_id) -> Person:
         person: Person = PersonService.retrieve(person_id)
         return person
+
+
+@api.route("/persons/<person_id>/connection")
+@api.param("start_date", "Lower bound of date range", _in="query")
+@api.param("end_date", "Upper bound of date range", _in="query")
+@api.param("distance", "Proximity to a given user in meters", _in="query")
+class ConnectionDataResource(Resource):
+    @responds(schema=ConnectionSchema, many=True)
+    def get(self, person_id) -> ConnectionSchema:
+        start_date: datetime = datetime.strptime(
+            request.args["start_date"], DATE_FORMAT
+        )
+        end_date: datetime = datetime.strptime(
+            request.args["end_date"], DATE_FORMAT)
+        distance: Optional[int] = request.args.get("distance", 5)
+
+        results = ConnectionService.find_contacts(
+            person_id=person_id,
+            start_date=start_date,
+            end_date=end_date,
+            meters=distance,
+        )
+        return results
