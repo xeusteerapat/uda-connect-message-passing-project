98806b0f25645de6d12e04de45ec9cc5c49653db
xeusteerapat <xeus085@gmail.com>
2022-05-31 09:22:03 +0700

    :memo: [Update] update API title

--- modules/persons-connection/app/__init__.py
+++ modules/persons-connection/app/__init__.py
@@ -15,7 +15,7 @@ def create_app(env=None):
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
-    api = Api(app, title="UdaConnect - Persons API", version="0.1.0")
+    api = Api(app, title="UdaConnect - Persons Connection API", version="0.1.0")
 
     CORS(app)  # Set CORS for development
 
