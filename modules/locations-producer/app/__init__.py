9a86dc2df7baaf1c3dee3fa7730dd09d06bb56fe
xeusteerapat <xeus085@gmail.com>
2022-05-31 09:26:15 +0700

    :wrench: [Update] config routes

--- modules/locations-producer/app/__init__.py
+++ modules/locations-producer/app/__init__.py
@@ -14,7 +14,7 @@ def create_app(env=None):
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
-    api = Api(app, title="UdaConnect - Locations Consumer API", version="0.1.0")
+    api = Api(app, title="UdaConnect - Locations Producer API", version="0.1.0")
 
     CORS(app)  # Set CORS for development
 
