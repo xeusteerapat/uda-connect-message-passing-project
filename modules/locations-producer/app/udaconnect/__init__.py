c7891fefe7f90b56379c8f257a46fead71e1f8a6
xeusteerapat <xeus085@gmail.com>
2022-06-04 20:01:54 +0700

    :fire: [Remove] refactor

--- modules/locations-producer/app/udaconnect/__init__.py
+++
@@ -1,4 +0,0 @@
-def register_routes(api, app, root="api"):
-    from app.udaconnect.controllers import api as udaconnect_location_api
-
-    api.add_namespace(udaconnect_location_api, path=f"/{root}")
